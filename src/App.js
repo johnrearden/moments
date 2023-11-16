cd23a6256897008e1c0670f656811916ae5a1237
johnrearden <isnocturnal@gmail.com>
2023-11-16 17:30:54 +0000

    Add registration form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar';
+import styles from "./App.module.css";
+import Container from 'react-bootstrap/Container';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import './api/axiosDefaults';
+import SignUpForm from './pages/auth/SignUpForm';
 
 function App() {
     return (
-        <div className="App">
-            <h1>Hello, world!</h1>
+        <div className={styles.App}>
+            <NavBar />
+            <Container className={styles.Main}>
+                <Switch>
+                    <Route exact path="/" render={() => <h1>Home Page</h1>} />
+                    <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+                    <Route exact path="/signup" render={() => <SignUpForm/>} />
+                </Switch>
+            </Container>
         </div>
     );
 }
