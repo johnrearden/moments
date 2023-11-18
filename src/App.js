5bef69cc6d9a6b7bd2d1d2d4b5544b1b642e1a42
johnrearden <isnocturnal@gmail.com>
2023-11-18 20:18:41 +0000

    Stop videos at axios interceptors!

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Container from 'react-bootstrap/Container';
 import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './api/axiosDefaults';
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
+
+
 
 function App() {
+
     return (
         <div className={styles.App}>
             <NavBar />
             <Container className={styles.Main}>
                 <Switch>
                     <Route exact path="/" render={() => <h1>Home Page</h1>} />
-                    <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
-                    <Route exact path="/signup" render={() => <SignUpForm/>} />
+                    <Route exact path="/signin" render={() => <SignInForm />} />
+                    <Route exact path="/signup" render={() => <SignUpForm />} />
                 </Switch>
             </Container>
         </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,31 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from "react-router-dom";
 import logo from '../assets/logo.png';
 import styles from "../styles/NavBar.module.css";
+import { useCurrentUser } from '../contexts/CurrentUserContext';
 
 const NavBar = () => {
+    const currentUser = useCurrentUser();
+    const loggedOutIcons = (
+        <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signin">
+                <i className="fas fa-sign-in-alt"></i>Sign In
+            </NavLink>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signup">
+                <i className="fas fa-user-plus"></i>Sign Up
+            </NavLink>
+        </>
+    );
+
+
+
+    const loggedInIcon = <>{currentUser?.username}</>
+
     return (
         <Navbar className={styles.NavBar} fixed="top" expand="md">
             <Container>
@@ -13,7 +36,7 @@ const NavBar = () => {
                         <img src={logo} alt="logo" height="45"></img>
                     </Navbar.Brand>
                 </NavLink>
-
+                {currentUser ? loggedInIcon : loggedOutIcons}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
@@ -22,18 +45,6 @@ const NavBar = () => {
                             activeClassName={styles.Active}
                             to="/">
                             <i className="fas fa-home"></i>Home
-                        </NavLink>
-                        <NavLink
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            to="/signin">
-                            <i className="fas fa-sign-in-alt"></i>Sign In
-                        </NavLink>
-                        <NavLink
-                            className={styles.NavLink}
-                            activeClassName={styles.Active}
-                            to="/signup">
-                            <i className="fas fa-user-plus"></i>Sign Up
                         </NavLink>
                     </Nav>
                 </Navbar.Collapse>
